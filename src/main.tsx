1aa95d7486e3afba43f4b3c0e6325c2e2b41b835
pdlsnartt <realap666@gmail.com>
2026-05-25 21:23:22 +0300

    Wrap app with AppStateProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AppStateProvider } from "./hooks/useAppState";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/web_cyberlearning">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <AppStateProvider>
+      <BrowserRouter basename="/web_cyberlearning">
+        <App />
+      </BrowserRouter>
+    </AppStateProvider>
+  </React.StrictMode>,
 );
