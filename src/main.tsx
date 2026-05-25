a4bc00bcf615394482b3c9984f979661fa73c051
pdlsnartt <realap666@gmail.com>
2026-05-25 21:03:32 +0300

    Fix router basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AppStateProvider } from "./hooks/useAppState";
 import "./styles/global.css";
 
-createRoot(document.getElementById("root")!).render(
-  
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter basename="/web_cyberlearning">
-      <AppStateProvider>
-        <App />
-      </AppStateProvider>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 );
